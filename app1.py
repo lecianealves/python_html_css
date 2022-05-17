53ab00867d4a2e46cd1cf693f852b424a2f2b9dd
lecianealves <lecianealves@hotmail.com>
2022-05-17 22:36:54 +0000

    App1 arquivo com comandos

---
+++ app1.py
@@ -0,0 +1,24 @@
+# Comandos:
+# -> pip install flask - Instala Flask
+# -> flask --version
+# Cria código 
+# -> flask run
+# Para rodar serviço flask run
+
+# Importa flask 
+from flask import Flask 
+
+# Cria app
+app = Flask("Hello")
+
+# Cria rota (URL)
+@app.route('/')# Outra rota para o mesmo destino
+@app.route("/hello") # Recurso do flask que usa URL chamando função
+def hello(): # Função a ser chamada
+    return "Hello World"
+
+# Nova rota 
+@app.route('/tchau')
+def tchau():
+    return "Tchau"
+
