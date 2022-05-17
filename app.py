82610dc9a6c83b8c19b15e59e02feebdf42072c3
lecianealves <lecianealves@hotmail.com>
2022-05-17 22:27:36 +0000

    Adição de comentários

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+# Para rodar serviço flask run
 # Importa flask 
 from flask import Flask 
 
-#Cria app
+# Cria app
 app = Flask("Hello")
 
 # Cria rota (URL)
-@app.route("/hello")
-def hello():
+@app.route('/')# Outra rota para o mesmo destino
+@app.route("/hello") # Recurso do flask que usa URL chamando função
+def hello(): # Função a ser chamada
     return "Hello World"
+
+# Nova rota 
+@app.route('/tchau')
+def tchau():
+    return "Tchau"
 
